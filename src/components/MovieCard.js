73a09b77b4d778c4a8774f082c50708074c5bdad
ghsong <84754325+ssonggary@users.noreply.github.com>
2023-09-04 14:46:53 +0900

    카드 클릭시 상세페이지로 넘어가기

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import Badge from "react-bootstrap/Badge";
+import { useNavigate } from "react-router-dom";
 
 const MovieCard = ({ item }) => {
+  const navigate = useNavigate();
   const { genreList } = useSelector((state) => state.movie);
 
   return (
     <div className="card" style={{ backgroundImage: "url(" + `https://www.themoviedb.org/t/p/w355_and_h200_multi_faces${item.poster_path}` + ")" }}>
-      <div className="overlay">
+      <button
+        onClick={() => {
+          navigate(`/movies/:${item.id}`);
+        }}
+        className="overlay">
         <h1>{item.title}</h1>
-        <div>
+        <div className="genre">
           {item.genre_ids.map((id) => (
             <Badge bg="danger" key={id}>
               {genreList.find((item) => item.id == id).name}
@@ -20,7 +26,7 @@ const MovieCard = ({ item }) => {
           <span>{item.vote_average}</span>
           <span>{item.adult ? "청불" : "Under 18"}</span>
         </div>
-      </div>
+      </button>
     </div>
   );
 };
